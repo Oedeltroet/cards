25360e6bb16e2270a0d5e7cea2c4184466854f4c
Teo Förste <teo-paul.foerste@informatik.hs-fulda.de>
2022-05-16 23:33:36 +0200

    changed server url

--- client/scripts/games/skip-bo.js
+++ client/scripts/games/skip-bo.js
@@ -2,5 +2,6 @@ function init() {
 
     document.body.innerHTML = "";
 
-    var socket = io("http://127.0.0.1:3000");
+    // var socket = io("localhost:3000");
+    var socket = io("http://cards.oedel.me:3000");
 };
