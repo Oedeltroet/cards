d7f432ef3365db154c78e51051b370e7071a46b3
Teo Förste <teo-paul.foerste@informatik.hs-fulda.de>
2022-05-16 23:55:06 +0200

    updated server code bugfix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-import { readFileSync } from "fs";
-import { createServer } from "https";
-import { Server } from "socket.io";
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
 
 const httpServer = createServer({
+
   key: readFileSync("/etc/letsencrypt/live/cards.oedel.me/privkey.pem"),
   cert: readFileSync("/etc/letsencrypt/live/cards.oedel.me/cert.pem"),
 });
@@ -10,8 +11,8 @@ const httpServer = createServer({
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
+
   console.log("A user has connected.");
-  // ...
 });
 
 httpServer.listen(3000);
