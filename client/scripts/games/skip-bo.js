6768b8d6187e88cf0e72168d4cd9fee7d3d3c6a9
Teo Förste <teofoerste@gmail.com>
2022-05-19 15:27:51 +0200

    server update

--- client/scripts/games/skip-bo.js
+++ client/scripts/games/skip-bo.js
@@ -1,7 +1,12 @@
 function init() {
 
-    document.body.innerHTML = "";
+    document.body.innerHTML = "<div id='menu'><button id='host' onclick='createGame()'>Create game</button></div>";
 
-    // var socket = io("localhost:3000");
+    //var socket = io("https://localhost:3000");
     var socket = io("https://cards.oedel.me:3000");
+
+    // function createGame() {
+
+    //     socket.emit("createGame");
+    // }
 };
--- server/index.js
+++ server/index.js
@@ -1,26 +1,37 @@
 const { readFileSync } = require("fs");
 const { createServer } = require("https");
 const { Server } = require("socket.io");
+var options = {};
 
-const httpServer = createServer({
+if (process.platform != "win32") {
 
-  key: readFileSync("/etc/letsencrypt/live/cards.oedel.me/privkey.pem"),
-  cert: readFileSync("/etc/letsencrypt/live/cards.oedel.me/cert.pem"),
-});
+  options = {
 
-const io = new Server(httpServer, {
-    
-    cors: {
-        
-        origin: "https://cards.oedel.me",
-        methods: ["GET", "POST"]
+    key: readFileSync("/etc/letsencrypt/live/cards.oedel.me/privkey.pem"),
+    cert: readFileSync("/etc/letsencrypt/live/cards.oedel.me/cert.pem"),
+  }
+}
+
+server = createServer(options);
+io = new Server(server, {
+  
+  cors: {
+      
+      origin: "https://cards.oedel.me",
+      methods: ["GET", "POST"]
     }
 });
+
 
 io.on("connection", (socket) => {
 
   console.log("A user has connected.");
 });
 
+// io.on("createGame", (socket) => {
+
+//   console.log("Creating new game.");
+// });
+
 console.log("Starting server...");
-httpServer.listen(3000);
+server.listen(3000);
